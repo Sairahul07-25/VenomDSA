cb690c820df5657256045a638f196017398c85f4
rahul <sai7rahul7@gmail.com>
2025-07-08 11:39:18 +0530

    Recursion basics 1

--- recursionBasics.java
+++ recursionBasics.java
@@ -2,10 +2,33 @@ import java.util.*;
 
 public class recursionBasics {
 
+	static void HeadprintName(int count){
+		if(count == 4){
+			return;
+		}
+		count++;
+		HeadprintName(count);
+		System.out.println("Rahul");
+		System.out.println(count);
+	}
+
+	static void TailprintName(int count){
+		if(count == 4){
+			return;
+		}
+		count++;
+		System.out.println("Rahul");
+		System.out.println(count);
+		TailprintName(count);
+	}
     public static void main(String[] args) {
         // Your code here
-        
-        
+        int count = 0;
+        System.out.println("Tail Recursion");
+    	TailprintName(count);
+    	System.out.println();
+    	System.out.println("Head Recursion");
+    	HeadprintName(count);
         
 
 
